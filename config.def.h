88a50eff7ef3bcce8da423d1e579ba9152fde904
ErreZac <raphael.zaccone@unige.it>
2022-04-04 18:25:29 +0200

    catppuccin

--- config.def.h
+++ config.def.h
@@ -10,14 +10,14 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#D8DEE9", "#1e222a" }, // 2E3440
-	[SchemeSel] = { "#2E3440", "#88C0D0"},
-	[SchemeSelHighlight] = { "#ECEFF4", "#434C5E"  },
-	[SchemeNormHighlight] = { "#2E3440", "#5E81AC" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-	[SchemeOutHighlight] = { "#88C0D0", "#00ffff" },
-	[SchemeMid] = { "#E5E9F0", "#3B4252" },
+	                     /*     fg         bg       */
+	[SchemeNorm]          = { "#D9E0EE", "#161320" }, // { "#ebdbb2", "#282828" }, // { "#D8DEE9", "#1e222a" }, // 2E3440
+	[SchemeSel]           = { "#1E1E2E", "#C9CBFF" }, // { "#1d2021", "#fabd2f" }, // { "#2E3440", "#88C0D0" },
+	[SchemeSelHighlight]  = { "#1E1E2E", "#F28FAD" }, // { "#1d2021", "#458588" }, // { "#ECEFF4", "#434C5E" },
+	[SchemeNormHighlight] = { "#1E1E2E", "#F5C2E7" }, // { "#1d2021", "#83a598" }, // { "#2E3440", "#5E81AC" },
+	[SchemeOut]           = { "#000000", "#00ffff" }, // { "#000000", "#00ffff" },
+	[SchemeOutHighlight]  = { "#000000", "#00ffff" }, // { "#88C0D0", "#00ffff" },
+	[SchemeMid]           = { "#D9E0EE", "#1E1E2E" }, // { "#ebdbb2", "#32302f" }, // { "#E5E9F0", "#3B4252" },
 };
 
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
