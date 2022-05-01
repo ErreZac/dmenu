5c93da84f83cc8c7ec5a344bebc4481aa39cb38b
ErreZac <raphael.zaccone@unige.it>
2022-05-01 14:01:25 +0200

    brighter

--- config.def.h
+++ config.def.h
@@ -12,7 +12,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	                     /*     fg         bg       */
 	[SchemeNorm]          = { "#a89984", "#282828" }, // { "#ebdbb2", "#282828" }, // { "#D8DEE9", "#1e222a" }, // 2E3440
-	[SchemeSel]           = { "#fbf1c7", "#458588" }, //C9CBFF { "#1d2021", "#fabd2f" }, // { "#2E3440", "#88C0D0" },
+	[SchemeSel]           = { "#fbf1c7", "#83a598" }, //C9CBFF { "#1d2021", "#fabd2f" }, // { "#2E3440", "#88C0D0" },
 	[SchemeSelHighlight]  = { "#1d2021", "#fabd2f" }, // { "#1d2021", "#458588" }, // { "#ECEFF4", "#434C5E" },
 	[SchemeNormHighlight] = { "#1d2021", "#83a598" }, // { "#1d2021", "#83a598" }, // { "#2E3440", "#5E81AC" },
 	[SchemeOut]           = { "#000000", "#00ffff" }, // { "#000000", "#00ffff" },
@@ -31,4 +31,4 @@ static unsigned int columns    = 2;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 1;
+static const unsigned int border_width = 4;
