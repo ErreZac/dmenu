5c8d6e47c29ea2d02f4be66626f98062261b0bd3
ErreZac <raphael.zaccone@unige.it>
2022-06-06 12:24:13 +0200

    most dark, now picom manages transpaency

--- colorthemes/onedark.h
+++ colorthemes/onedark.h
@@ -1,9 +1,9 @@
 static const char *colors[SchemeLast][2] = {
 	                     /*     fg         bg       */
-	[SchemeNorm]          = { "#abb2bf", "#0e1013" }, 
-	[SchemeSel]           = { "#0e1013", "#c678dd" }, 
-	[SchemeSelHighlight]  = { "#0e1013", "#61afef" }, 
-	[SchemeNormHighlight] = { "#0e1013", "#98c379" }, 
+	[SchemeNorm]          = { "#abb2bf", "#040506" }, 
+	[SchemeSel]           = { "#040506", "#c678dd" }, 
+	[SchemeSelHighlight]  = { "#040506", "#61afef" }, 
+	[SchemeNormHighlight] = { "#040506", "#98c379" }, 
 	[SchemeOut]           = { "#000000", "#00ffff" }, 
 	[SchemeOutHighlight]  = { "#000000", "#00ffff" }, 
 	[SchemeMid]           = { "#c8ccd4", "#353b45" }, 
--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
-static const unsigned int alpha = 0xff*0.89;
+static const unsigned int alpha = 0xff;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Victor Mono Nerd Font:size=22:style=Regular"
