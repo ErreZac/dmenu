187ec8d0e0ce480e2ed3a7e7d4260ee268c2c24f
ErreZac <raphael.zaccone@unige.it>
2022-06-22 11:50:44 +0200

    fixed blue

--- colorthemes/catppuccin.h
+++ colorthemes/catppuccin.h
@@ -1,7 +1,7 @@
 static const char *colors[SchemeLast][2] = {
 	                     /*     fg         bg       */
 	[SchemeNorm]          = { "#cdd6f4", "#11111b" },
-	[SchemeSel]           = { "#11111b", "#74c7ec" },
+	[SchemeSel]           = { "#11111b", "#89b4fa" },
 	[SchemeSelHighlight]  = { "#11111b", "#f9e2af" },
 	[SchemeNormHighlight] = { "#11111b", "#cba6f7" },
 	[SchemeOut]           = { "#000000", "#00ffff" },
