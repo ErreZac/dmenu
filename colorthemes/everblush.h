c3cc86db6595395eecda603cf8e52140b5ccb1fd
ErreZac <raphael.zaccone@unige.it>
2022-08-06 22:20:42 +0200

    blue everblush

--- colorthemes/everblush.h
+++ colorthemes/everblush.h
@@ -1,7 +1,7 @@
 static const char *colors[SchemeLast][2] = {
 	                     /*     fg         bg       */
 	[SchemeNorm]          = { "#dadada", "#141b1e" },
-	[SchemeSel]           = { "#141b1e", "#e5c76b" },
+	[SchemeSel]           = { "#141b1e", "#67b0e8" },
 	[SchemeSelHighlight]  = { "#141b1e", "#6cbfbf" },
 	[SchemeNormHighlight] = { "#141b1e", "#c47fd5" },
 	[SchemeOut]           = { "#000000", "#00ffff" },
