f95a001b837e50fbf2bb1269ef7def486f9b3739
ErreZac <raphael.zaccone@unige.it>
2022-09-08 14:19:49 +0200

    minimal things

--- colorthemes/gruvbox.h
+++ colorthemes/gruvbox.h
@@ -1,7 +1,7 @@
 static const char *colors[SchemeLast][2] = {
 	                     /*     fg         bg       */
 	[SchemeNorm]          = { "#a89984", "#0e1011" },
-	[SchemeSel]           = { "#0e1011", "#fbf1c7" },
+	[SchemeSel]           = { "#0e1011", "#ebdbb2" },
 	[SchemeSelHighlight]  = { "#0e1011", "#d3869b" },
 	[SchemeNormHighlight] = { "#0e1011", "#fabd2f" },
 	[SchemeOut]           = { "#000000", "#00ffff" },
