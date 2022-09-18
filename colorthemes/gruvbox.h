b46e78a10dec7454680aefe50ca21b1b567a244c
ErreZac <raphael.zaccone@unige.it>
2022-09-18 14:20:08 +0200

    minor color tweaks

--- colorthemes/gruvbox.h
+++ colorthemes/gruvbox.h
@@ -2,7 +2,7 @@ static const char *colors[SchemeLast][2] = {
 	                     /*     fg         bg       */
 	[SchemeNorm]          = { "#a89984", "#0e1011" },
 	[SchemeSel]           = { "#0e1011", "#ebdbb2" },
-	[SchemeSelHighlight]  = { "#0e1011", "#d3869b" },
+	[SchemeSelHighlight]  = { "#0e1011", "#83a598" },
 	[SchemeNormHighlight] = { "#0e1011", "#fabd2f" },
 	[SchemeOut]           = { "#000000", "#00ffff" },
 	[SchemeOutHighlight]  = { "#000000", "#00ffff" },
