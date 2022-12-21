c9bd60a1885c30e2bd073130fb48df4675cc70e8
ErreZac <raphael.zaccone@unige.it>
2022-12-21 08:28:59 +0100

    theme

--- colorthemes/gruvbox.h
+++ colorthemes/gruvbox.h
@@ -1,9 +1,9 @@
 static const char *colors[SchemeLast][2] = {
 	                     /*     fg         bg       */
 	[SchemeNorm]          = { "#a89984", "#0e1011" },
-	[SchemeSel]           = { "#0e1011", "#ebdbb2" },
+	[SchemeSel]           = { "#0e1011", "#fabd2f" },
 	[SchemeSelHighlight]  = { "#0e1011", "#83a598" },
-	[SchemeNormHighlight] = { "#0e1011", "#fabd2f" },
+	[SchemeNormHighlight] = { "#0e1011", "#ebdbb2" },
 	[SchemeOut]           = { "#000000", "#00ffff" },
 	[SchemeOutHighlight]  = { "#000000", "#00ffff" },
 	[SchemeMid]           = { "#0e1011", "#a89984" },
