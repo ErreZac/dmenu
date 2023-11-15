4809dc15dd6326184fc9374e9d5012e897ac2d36
ErreZac <raphael.zaccone@unige.it>
2023-11-15 12:39:38 +0100

    moon

---
+++ colorthemes/rosepine_moon.h
@@ -0,0 +1,11 @@
+static const char *colors[SchemeLast][2] = {
+	                     /*     fg         bg       */
+	[SchemeNorm]          = { "#908caa", "#232136" },
+	[SchemeSel]           = { "#e0def4", "#3e8fb0" },
+	[SchemeSelHighlight]  = { "#232136", "#f6c177" },
+	[SchemeNormHighlight] = { "#232136", "#c4a7e7" },
+	[SchemeOut]           = { "#000000", "#00ffff" },
+	[SchemeOutHighlight]  = { "#000000", "#00ffff" },
+	[SchemeMid]           = { "#908caa", "#2a273f" },
+};
+
